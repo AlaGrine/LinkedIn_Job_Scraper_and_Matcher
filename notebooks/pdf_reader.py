1c4bf9a8e6138c8fcdd627706eb9adfe6b7b6a32
Ala GRINE <alaeddine.grine@gmail.com>
2023-11-26 17:27:29 +0100

    Add notebooks

---
+++ notebooks/pdf_reader.py
@@ -0,0 +1,26 @@
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def pdf_miner(file_path):
+    output_string = StringIO()
+    with open(file_path, "rb") as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+
+    resume_txt = output_string.getvalue()  # str type
+    return resume_txt
